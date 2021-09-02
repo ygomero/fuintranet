b805d785f0767e0b2be78ec9c270a31e65e36b26
RodrigoHuaranga <33135741+RodrigoHuaranga@users.noreply.github.com>
2021-09-02 18:52:42 -0500

    feat : page not found

--- app/shared/core/View.php
+++ app/shared/core/View.php
@@ -5,10 +5,16 @@ class View
     private static $path_views = DIR_APP.DS."web".DS."modules";
     private static $path_main = DIR_APP.DS."web".DS."shared".DS."layout".DS."index.html";
 
-    static function render($view){
-        $main = file_get_contents(self::$path_main);
-        $content = file_get_contents(self::$path_views.DS.$view.DS.'main.html');
-        $main = str_replace("<content></content>",$content,$main);
-        return $main;
+    static function render($view,$alone = false){
+        if($alone){
+            $content = file_get_contents(self::$path_views.DS.$view.DS.'main.html');
+            return $content;
+        }
+        else{
+            $main = file_get_contents(self::$path_main);
+            $content = file_get_contents(self::$path_views.DS.$view.DS.'main.html');
+            $main = str_replace("<content></content>",$content,$main);
+            return $main;
+        }
     }
 }
