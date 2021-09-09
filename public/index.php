cf5ffc8a4fe93936f411b49cf10c46f7f0eb4c53
RodrigoHuaranga <33135741+RodrigoHuaranga@users.noreply.github.com>
2021-09-09 16:59:26 -0500

    phpspread

--- public/index.php
+++ public/index.php
@@ -1,4 +1,5 @@
 <?php
+require '../vendor/autoload.php';
 // Usaría require siempre que el código sea importante (Funciones reutilizables de PHP, configuraciones…), 
 //mientras que include lo usaría en casos en los que el código no es vital para la ejecución del script (cabeceras y pies HTML o similares).
 include('../app/shared/common/defines.php');
